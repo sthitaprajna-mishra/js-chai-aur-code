5192e9c79ebe6dba352d2eefda0d84fd35de8dbd
Sthitaprajna Mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2025-05-17 03:07:42 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+// Number => 2^53
+// BigInt
+// String => ""
+// Boolean => true/false
+// Null => standalone value
+// Undefined => value aint assigned as of now
+// Symbol => unique
+
+// object
+
+console.log(typeof name)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
