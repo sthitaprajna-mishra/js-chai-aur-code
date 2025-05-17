446ffaf7c96c7ef4ad14128cbeacb9e1715e7f0d
Sthitaprajna Mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2025-05-17 02:40:49 +0000

    01_variables commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+let accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "password21"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
