f97db625d770449844d911bbcde86532c8bc8ce3
Sthitaprajna Mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2025-05-17 03:39:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = undefined // null //"33hgd"
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber) 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "efgweg" => true
+
+let someNumber = 33
+let stringNumber = String(33)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
