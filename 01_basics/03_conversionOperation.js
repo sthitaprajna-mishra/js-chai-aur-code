47c4c7083a621795a404b9d036d5faf9a9368644
Sthitaprajna Mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2025-05-17 03:53:22 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,6 +22,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(33)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************************ Operations ************************
+
+let value  = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+// console.table([num1, num2, num3])
+
+let gameCounter = 100
+gameCounter++
+console.log(++gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
 
